e04c9fb44ea634ec6697e82377b5112f94134ead
Stakul <stakul@kth.se>
2017-02-11 19:17:30 +0100

    v0.1

---
+++ echoserver.erl
@@ -0,0 +1,19 @@
+-module(echoserver).
+
+-export([start/0,loop/0,print/1,stop/0]).
+
+start()->
+    register(echo, spawn(echoserver,loop,[])).
+
+print(Term)->
+    echo ! Term.
+
+stop()->
+    echo ! stop.
+
+loop() ->
+  receive
+    stop -> true;
+    Msg -> io:format("~w~n", [Msg]),
+    loop()
+  end.
